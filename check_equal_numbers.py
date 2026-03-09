dd466af9bff58c7c80e947191c4e70666af233fa
Duke Buerano <dukebueranoo@gmail.com>
2026-03-09 21:50:10 +0800

    final commit

--- check_equal_numbers.py
+++ check_equal_numbers.py
@@ -12,3 +12,8 @@ def check_if_equal():
     if first_number == second_number:
         # If they are the same, print "Equal" in green text
         print("\033[92mEqual\033[0m")
+
+        # This part tells the computer to actually start the program
+if __name__ == "__main__":
+    # Run the command we made at the top
+    check_if_equal()
