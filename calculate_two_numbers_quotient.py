23753a4fb9d3a3cf8fa96d99861069e7ffa065e8
Duke Buerano <dukebueranoo@gmail.com>
2026-03-09 22:10:04 +0800

    final commit

--- calculate_two_numbers_quotient.py
+++ calculate_two_numbers_quotient.py
@@ -19,3 +19,8 @@ def calculate_quotient():
     else:
         # Print an error message in red text so the user knows what went wrong
         print("\033[91mCannot divide by zero.\033[0m")
+
+        # This part tells the computer to actually start the program
+if __name__ == "__main__":
+    # Run the division command we made at the top
+    calculate_quotient()
