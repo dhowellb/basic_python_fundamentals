7d08e2688d807bfadc680807e5b186a106e87382
Duke Buerano <dukebueranoo@gmail.com>
2026-03-09 21:54:55 +0800

    first commit

---
+++ calculate_two_numbers_sum.py
@@ -0,0 +1,13 @@
+# Make a command to add two numbers together
+def calculate_sum():
+    # Ask the user for the first number and let them use decimals (float)
+    first_number = float(input("\033[96mEnter the first number: \033[0m"))
+    
+    # Ask the user for the second number
+    second_number = float(input("\033[96mEnter the second number: \033[0m"))
+    
+    # Add the first number and second number together and save it
+    total_sum = first_number + second_number
+
+    # Print the final answer in green text
+    print("\033[92mThe sum is:\033[0m", total_sum)
